ed111ce729455be2cd17d5ab31e024dcc749595d
normi hussin <normi.hussin@paynet.my>
2021-12-20 11:49:09 +0800

    format-issue is fixed

---
+++ JS calc.js
@@ -0,0 +1,16 @@
+let addition = function(x, y) {
+    return x + y;
+}
+let subtraction = function(x, y) {
+    return x - y;
+}
+let multiplication = function(x, y) {
+    return x * y;
+}
+let division = function(x, y) {
+    return x / y;
+}
+console.log("Addition:", addition(20, 10))
+console.log("Subtraction:", subtraction(20, 10))
+console.log("Multiplication:", multiplication(20, 10))
+console.log(division(20, 10))
